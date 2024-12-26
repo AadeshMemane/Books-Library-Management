d06a2ccb60ce89155129fbb40bf22911eec6eb78
Aadesh Memane <aadeshmemane1504@gmail.com>
2024-12-26 19:35:08 +0530

    Implement feature that does not allow to borrow which does not exist

--- src/book.js
+++ src/book.js
@@ -16,6 +16,9 @@ Library.prototype.addBook = function (isbn, title, author, year) {
 //Function for borrowing a available book
 Library.prototype.borrowBook = function (isbn) {
   const book = this.books.find((book) => book.isbn === isbn)
+  if (!book) {
+    throw new Error('Book not found.')
+  }
   book.available = false
 }
 
